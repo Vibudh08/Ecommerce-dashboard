6c2a69c7dfbeca9cc2cbb480a9fb1482428fedc2
Vibudh08 <vibudhrathore8@gmail.com>
2024-05-26 17:14:17 +0530

    logout

--- ecomm-frontend/src/Header.js
+++ ecomm-frontend/src/Header.js
@@ -1,13 +1,20 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  let navigate = useNavigate()
+  const logout = ()=>{
+    localStorage.clear()
+    navigate('/login')
+  }
+  const user = JSON.parse(localStorage.getItem("user-info"));
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
-          <Nav className="mr-auto nav_bar_wrapper">
+          <Nav className="me-auto nav_bar_wrapper">
             {localStorage.getItem("user-info") ? (
               <>
                 <Link to="/add">Add Product</Link>
@@ -19,6 +26,14 @@ function Header() {
                 <Link to="/register">Register</Link>
               </>
             )}
+          </Nav>
+          <Nav>
+            {localStorage.getItem("user-info") ? (
+              <NavDropdown title={user.name}>
+                <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </NavDropdown>
+            ) : null}
           </Nav>
         </Container>
       </Navbar>
