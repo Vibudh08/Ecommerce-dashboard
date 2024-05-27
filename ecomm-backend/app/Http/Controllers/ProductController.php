35bfdee6c79917b4053703752858803ac9d1c9b1
Vibudh08 <vibudhrathore8@gmail.com>
2024-05-28 01:03:14 +0530

    search

--- ecomm-backend/app/Http/Controllers/ProductController.php
+++ ecomm-backend/app/Http/Controllers/ProductController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    function addProduct(Request $req){
+    function addProduct(Request $req)
+    {
         $product = new Product;
         $product->name = $req->name;
         $product->description = $req->description;
@@ -15,33 +17,42 @@ class ProductController extends Controller
         $product->save();
         return $product;
     }
-    function list(){
+    function list()
+    {
         return Product::all();
     }
-    function delete($id){
-        $result = Product::where('id',$id)->delete();
-        if ($result){
-            return ["result"=>"Product has been deleted"];
+    function delete($id)
+    {
+        $result = Product::where('id', $id)->delete();
+        if ($result) {
+            return ["result" => "Product has been deleted"];
         }
     }
-    function getProduct($id){
+    function getProduct($id)
+    {
         $result = Product::find($id);
         return $result;
     }
-    function updateProduct($id, Request $req){
+    function updateProduct($id, Request $req)
+    {
         $product = Product::find($id);
         // return $req->input();
         $product->name = $req->name;
         $product->description = $req->description;
         $product->price = $req->price;
-        if($req->file('file')){
+        if ($req->file('file')) {
             $product->file_path = $req->file("file")->store("products");
         }
         $product->save();
         return $product;
-    }   
-    function search($key){
-        return Product::where('name','LIKE',"%$key%")->get();
+    }
+    function search($key)
+    {
+        if($key){
+            return Product::where('name', 'LIKE', "%$key%")->get();
+        }
+        else{
+            return "";
+        }
     }
 }
-
--- ecomm-frontend/src/SearchProduct.js
+++ ecomm-frontend/src/SearchProduct.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
 import Header from "./Header";
-import { useNavigate } from "react-router-dom";
+import { Table } from "react-bootstrap";
 
 function SearchProduct() {
+  const [data, setData] = useState("");
+  const search = async (key) => {
+    if(key.length>0){
+
+        let res = await axios.get("http://127.0.0.1:8000/api/search/" + key);
+        // console.log(res.data);
+        setData(res.data);
+    }
+  };
   return (
     <>
       <Header />
@@ -14,8 +23,44 @@ function SearchProduct() {
         <input
           type="text"
           className="form-control"
+          onChange={(e) => search(e.target.value)}
           placeholder="Search Product"
         />
+      </div>
+      <div className="col-sm-8 offset-sm-2">
+        <br />
+        {
+            data?
+            <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Description</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(data) &&
+              data.map((d, i) => (
+                <tr key={i}>
+                  <th>{d.id}</th>
+                  <th>{d.name}</th>
+                  <th>
+                    <img
+                      style={{ width: 100, height: 100 }}
+                      src={"http://127.0.0.1:8000/" + d.file_path}
+                    />
+                  </th>
+                  <th>{d.description}</th>
+                  <th>{d.price}</th>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+        :"Write the name of product which you want to search"
+    }
       </div>
     </>
   );
